5271c2b8dfb8a4bb2b5551cc237eedd67f49a408
turgut5534 <turgutsalgin5534@gmail.com>
2025-12-20 23:14:54 +0100

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -18,6 +19,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+    const config = new DocumentBuilder()
+    .setTitle('Admin API')
+    .setDescription('API for managing portfolio content')
+    .setVersion('1.0')
+    .addBearerAuth() // if you use JWT auth
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
 
   await app.listen(process.env.PORT ?? 3010);
 }
